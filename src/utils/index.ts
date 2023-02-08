0107092e160b48e89b27d612c360b7a8f509d163
Joseph Chingalo <profschingalo@gmail.com>
2023-02-08 17:23:37 +0300

    feat: 🎸 add default utils for the app

---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export * from './app-util';
+export * from './excel-util';
+export * from './file-util';
+export * from './http-util';
+export * from './logs-util';
+export * from './email-notification-util';
