863845e09c7994be32af39176ff1bd5763f01ed1
Joseph Chingalo <profschingalo@gmail.com>
2023-02-10 11:08:36 +0300

    feat: 🎸 get formatted indicators

--- src/app/app-process.ts
+++ src/app/app-process.ts
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import { appConfig } from '../configs';
+import { INDICATOR_TYPES } from '../constants/indicator-constant';
 import { AnalyticsUtil, IndicatorUtil, LogsUtil } from '../utils';
 
 export class AppProcess {
@@ -21,11 +22,24 @@ export class AppProcess {
 
   async startProcess() {
     try {
-      // TODO logics for data
-      console.log('Todo handle logics for the script');
-      // get indicator details by indicator types -> number
-      // get indicator with similar data elements as numberator and denominator
-      // get indicator analyics values
+      const formattedIndicators = [];
+      const indicators = await this._indicatorUtil.getAllIndicatorsByType(
+        INDICATOR_TYPES
+      );
+      const indicatorDataObjects =
+        await this._analyticsUtil.getAnalyticalDataFromServer(
+          _.flattenDeep(_.map(indicators, (indicator) => indicator.id))
+        );
+      for (const indicator of indicators) {
+        if (indicatorDataObjects[indicator.id]) {
+          formattedIndicators.push({
+            ...indicator,
+            name : `${indicator.name}`.trim(),
+            value: indicatorDataObjects[indicator.id]
+          });
+        }
+      }
+      console.log(JSON.stringify(formattedIndicators));
     } catch (error: any) {
       await new LogsUtil().addLogs(
         'error',
